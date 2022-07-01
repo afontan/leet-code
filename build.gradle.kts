bdcf9e949df6cd3e1ec782fe78d72112b6370d00
Alejandro Fontán <alejandro.fontan@glovoapp.com>
2022-07-01 22:27:04 +0200

    clean build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,80 +1,24 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinVersion = "1.3.72"
-val jUnitVersion = "5.4.2"
-val kluentVersion = "1.51"
-val spekVersion = "2.0.5"
-val khttpVersion = "0.1.0"
-val klaxonVersion = "5.2"
-val jacocoVersion = "0.8.5"
-val gradleWrapperVersion = "6.3"
-
 plugins {
-    application
-    kotlin("jvm") version "1.3.72"
-    jacoco
+    kotlin("jvm") version "1.6.20"
 }
 
+group = "org.afontan"
+version = "1.0-SNAPSHOT"
+
 repositories {
-    mavenLocal()
     mavenCentral()
-    maven("https://jitpack.io")
-    jcenter()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("reflect"))
-    implementation("com.github.jkcclemens:khttp:$khttpVersion")
-    implementation("com.beust:klaxon:$klaxonVersion")
-
     testImplementation(kotlin("test"))
-    testImplementation(kotlin("test-junit"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
-    testImplementation("org.amshove.kluent:kluent:$kluentVersion")
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
-    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion")
-}
-
-jacoco {
-    toolVersion = jacocoVersion
-}
-
-tasks.jacocoTestReport {
-    reports {
-        xml.isEnabled = true
-        csv.isEnabled = true
-        html.isEnabled = true
-    }
 }
 
 tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
-    useJUnitPlatform {
-        includeEngines("junit-jupiter", "spek2")
-    }
-
-    testLogging {
-        exceptionFormat = TestExceptionFormat.FULL
-        events("passed", "failed", "skipped")
-    }
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
-        apiVersion = "1.3"
-        languageVersion = "1.3"
-    }
-}
-
-tasks.wrapper {
-    gradleVersion = gradleWrapperVersion
+    kotlinOptions.jvmTarget = "1.8"
 }
