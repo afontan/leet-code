5ac75a93356693ba8ce2d406637e0d3f3d63e270
Alejandro Fontán <alejandro.fontan@glovoapp.com>
2022-07-02 02:14:46 +0200

    enable coverage report

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    jacoco
 }
 
-group = "org.afontan"
+group = "io.github.afontan"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -15,8 +16,33 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+jacoco {
+    toolVersion = "0.8.5"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = true
+        html.isEnabled = true
+    }
+}
+
 tasks.test {
-    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+    useJUnitPlatform {
+        includeEngines("junit-jupiter", "spek2")
+    }
+
+    testLogging {
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        events("passed", "failed", "skipped")
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<KotlinCompile> {
