51ada6b753ecf205957dd62fcc5983c818cb87fb
Alejandro Fontán <afontan91@gmail.com>
2025-07-28 09:21:41 +0200

    Update SolutionTest.kt

--- src/test/kotlin/problem_104/SolutionTest.kt
+++ src/test/kotlin/problem_104/SolutionTest.kt
@@ -20,7 +20,7 @@ class SolutionTest {
 
     @Test
     fun `Description test case 2`() {
-        println("Hola mundo2")
+        println("Hola mundo3")
         val tree = TreeNode(1).apply {
             right = TreeNode(2)
         }
